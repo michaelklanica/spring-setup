a00a7218cf48bff085729e99a7875a4582a2dccc
Michael Klanica <michael.david.klanica@gmail.com>
2020-11-24 08:11:55 -0600

    initial commit for Spring setup/practice

---
+++ src/test/java/com/example/springsetup/SpringSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
